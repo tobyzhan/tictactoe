3585289e0cd0d603faedca4ac0b067feed9aa6bd
minahAHh <88470019+minahAHh@users.noreply.github.com>
2021-08-04 21:01:54 -0700

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,22 +1,19 @@
 # Tic Tac Toe
 
 class TicTacToe:
-
-    def drawBoard(board):
-    # This function prints out the board that it was passed.
-    # "board" is a list of 10 strings representing the board (ignore index 0)
-        print('   |   |')
-        print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-        print('   |   |')
-        print('-----------')
-        print('   |   |')
-        print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-        print('   |   |')
-        print('-----------')
-        print('   |   |')
-        print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-        print('   |   |')
-
+   def drawBoard(board):
+    theBoard = '''
+    _______________________
+   |       |       |       | 
+   |   7   |   8   |   9   |
+   |_______|_______|_______|
+   |       |       |       |
+   |   4   |   5   |   6   |
+   |_______|_______|_______|
+   |       |       |       |
+   |   1   |   2   |   3   |
+   |_______|_______|_______|'''
+   
     def inputPlayerLetter():
         letter = ''
         while not (letter == 'X' or letter == 'O'):
